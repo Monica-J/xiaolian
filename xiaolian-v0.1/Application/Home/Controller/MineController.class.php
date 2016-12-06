4378eee1dee005e2573cf92f6075434798611525
liwenlong <466735021@qq.com>
2016-12-06 16:59:40 +0800

    123

--- xiaolian-v0.1/Application/Home/Controller/MineController.class.php
+++ xiaolian-v0.1/Application/Home/Controller/MineController.class.php
@@ -2,7 +2,7 @@
 namespace Home\Controller;
 use Think\Controller;
 class MineController extends Controller {
-    public function index(){
+    public function mine(){
        $this->display();
     }
    
---
+++ xiaolian-v0.1/Application/Runtime/Cache/Home/8f001e5c9180a25a1619bf0762fd2361.php
@@ -0,0 +1,51 @@
+<?php if (!defined('THINK_PATH')) exit();?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <title>首页</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"> 
+    <link rel="stylesheet" href="/xiaoliangit/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" />
+    <script src="/xiaoliangit/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
+    <script src="/xiaoliangit/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
+
+<style type="text/css">
+	*{margin:0;padding:0;list-style:none;border:0;}
+	p{color:#8E8E8E;font-size:12px;font-weight:200;}
+</style>
+</head>
+
+<body>
+	<div data-role="page">
+		<div data-role="header" style="background-color:#6fd7ca;color:#fff;">
+			<a href="show.html" data-role="button" data-icon="arrow-l" data-iconpos="notext"></a>
+			<h1 style="font-weight:200;">更多活动</h1>
+		</div>
+
+		<div data-role="content">
+			<div class="panel-1" style="margin-bottom: -10px">
+				<a href="active-specific.html" style="text-decoration:none;">
+                    <span><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/show-activity.png" style="width:100%">
+                    <p style="color:#8E8E8E;">河北师范大学2016年度迎新送老晚会将于9月23日在软件楼门口举行，届时将会有精彩的演出，欢迎同学们前来观看</p>
+                	</span>
+                </a>
+			</div>
+			<img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/show-line.png" style="width:1500px;height:1px;margin-left:-20px;">
+			<div class="panel-2" style="margin-bottom: -10px; margin-top: 10px;">
+				<a href="active-specific.html" style="text-decoration:none;">
+                    <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/show-activity.png" style="width:100%">
+                    <p style="color:#8E8E8E;">河北师范大学2016年度迎新送老晚会将于9月23日在软件楼门口举行，届时将会有精彩的演出，欢迎同学们前来观看</p>
+                </a>
+			</div>
+			<img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/show-line.png" style="width:1500px;height:1px;margin-left:-20px;">
+		</div>
+
+		<div data-role="footer" data-position="fixed">
+			
+		</div>
+	</div>
+
+</body>
+
+
+</html>
