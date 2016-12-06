9bcf6d78aadf176a0a856707d27b5fbe09d49bee
liwenlong <466735021@qq.com>
2016-12-06 17:10:46 +0800

    1

--- xiaolian-v0.1/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ xiaolian-v0.1/Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -1,6 +1,6 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html> 
 <html> 
-<head> 
+  <head> 
     <meta charset="utf-8" />
     <title>首页</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
@@ -71,7 +71,7 @@ $(document).ready(function(){
     
 });
 </script>
-</head>
+  </head>
 
 <body> 
     <div data-role="page" theme="b"> 
@@ -105,6 +105,7 @@ $(document).ready(function(){
                 <!--推荐用户-->
                 <div class="ui-grid-c" style="margin-top: 10px;">
                     <div class="ui-block-a">
+<<<<<<< HEAD
                         <a href="user-data.html"><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-user.png" style="width: 60px;height: 60px;"></a>
                     </div>
                     <div class="ui-block-b">
@@ -115,6 +116,18 @@ $(document).ready(function(){
                     </div>
                     <div class="ui-block-d">
                         <a href="user-data.html"><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-user.png" style="width: 60px;height: 60px;"></a>
+=======
+                        <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/user-data.html"><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-user.png" style="width: 60px;height: 60px;"></a>
+                    </div>
+                    <div class="ui-block-b">
+                        <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/user-data.html"><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-user.png" style="width: 60px;height: 60px;"></a>
+                    </div>
+                    <div class="ui-block-c">
+                        <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/user-data.html"><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-user.png" style="width: 60px;height: 60px;"></a>
+                    </div>
+                    <div class="ui-block-d">
+                        <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/user-data.html"><img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-user.png" style="width: 60px;height: 60px;"></a>
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                     </div>
                 </div>
             </div>
@@ -127,12 +140,20 @@ $(document).ready(function(){
                         <h4 style="font-family:'黑体';font-weight:bold;">热门活动</h4>
                     </div>
                     <div class="ui-block-b" style="margin: 0 -5px 5px 0;">
+<<<<<<< HEAD
                         <a href="activity--more.html" style="float:right;color:#6fd7ca;font-size:14px;text-decoration:none;"><span>更多<img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-more.png" style="width:14px;height:14px;"></span></a>
+=======
+                        <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/activity-more.html" style="float:right;color:#6fd7ca;font-size:14px;text-decoration:none;"><span>更多<img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-more.png" style="width:14px;height:14px;"></span></a>
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                     </div>
                 </div>
                 <!--热门活动推荐-->
                 <div class="panel-2-1"><!--应该做成轮播效果 但目前还没有实现-->
+<<<<<<< HEAD
                     <a href="activity-specific.html" style="text-decoration:none;"><span>
+=======
+                    <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/activity-specific.html" style="text-decoration:none;"><span>
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                         <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/index-activity.png" style="width:100%">
                         <p style="color:#8E8E8E;font-size:12px;font-family:'微软雅黑';font-weight:200;">河北师范大学2016年度迎新送老晚会将于9月23日在软件楼门口举行，届时将会有精彩的演出，欢迎同学们前来观看</p>
                     </span></a>
@@ -147,31 +168,51 @@ $(document).ready(function(){
             <div data-role="navbar">
               <ul>
                 <li >
+<<<<<<< HEAD
                     <a href="index.html" style="background-color:#6fd7ca;">
+=======
+                    <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/index/index.html" style="background-color:#6fd7ca;">
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                         <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/shouye.png" style="width:24px; height:24px;">
                         <p style="margin:1px 0px -5px 0px;color: #fff;font-size:12px;font-weight:100;font-family:'微软雅黑';">首页</p>
                     </a>
                 </li>
                 <li>
+<<<<<<< HEAD
                     <a href="task.html"  style="background-color:#6fd7ca;">
+=======
+                    <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/task/task.html"  style="background-color:#6fd7ca;">
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                         <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/renwu.png" style="width:24px; height:24px;">
                         <p style="margin:1px 0px -5px 0px;color: #fff;font-size:12px;font-weight:100;font-family:'微软雅黑';">匹配任务</p>
                     </a>
                 </li>
                 <li>
+<<<<<<< HEAD
                     <a href="kankan.html"  style="background-color:#6fd7ca;">
+=======
+                    <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/kankan/kankan.html"  style="background-color:#6fd7ca;">
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                         <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/kankan.png" style="width:24px; height:24px;">
                         <p style="margin:1px 0px -5px 0px;color: #fff;font-size:12px;font-weight:100;font-family:'微软雅黑';">看看</p>
                     </a>
                 </li>
                 <li>
+<<<<<<< HEAD
                     <a href="foot.html"  style="background-color:#6fd7ca;">
+=======
+                    <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/foot/foot.html"  style="background-color:#6fd7ca;">
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                         <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/zuji.png" style="width:24px; height:24px;">
                         <p style="margin:1px 0px -5px 0px;color: #fff;font-size:12px;font-weight:100;font-family:'微软雅黑';">足迹</p>
                     </a>
                 </li>
                 <li>
+<<<<<<< HEAD
                     <a href="mine.html"  style="background-color:#6fd7ca;">
+=======
+                    <a href="/xiaoliangit/xiaolian-v0.1/index.php/Home/mine/mine.html"  style="background-color:#6fd7ca;">
+>>>>>>> 81c3ae0a68e941c4ac1ced05089dbd71632b69ab
                         <img src="/xiaoliangit/xiaolian-v0.1/Public/front/images/wode.png" style="width:24px; height:24px;">
                         <p style="margin:1px 0px -5px 0px;color: #fff;font-size:12px;font-weight:100;font-family:'微软雅黑';">我的</p>
                     </a>
