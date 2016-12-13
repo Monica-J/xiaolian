de9099ecc06a3cf3d37eda04b642e0585503afff
yuexiaoxing <1071125135@qq.com>
2016-12-13 16:01:40 +0800

    1

---
+++ xiaolian-v0.1/Application/Runtime/Cache/Home/15517bcb3397c1f47fbf142b2d668f25.php
@@ -0,0 +1,84 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+ <head>
+	 <title>注册界面</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="/xiaolian/xiaolian-v0.1/Public/front/css/shezhi-style.css">   
+    <link rel="stylesheet" href="/xiaolian/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" />
+    <script src="/xiaolian/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
+    <script src="/xiaolian/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+    <div data-role="page">
+               <div data-role="header" style="background-color:#6fd7ca; color:white;">
+			     <a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/mine.html" onclick="location.href='/xiaolian/xiaolian-v0.1/index.php/Home/mine/mine.html'"  data-icon="arrow-l" data-iconpos="notext"></a>
+			        <h1 style="font-weight:200;">完善个人资料</h1>
+		       </div>
+        <div data-role="content">
+            <div data-role="controlgroup" data-type="vertical">
+            <div style="text-align:center">
+
+		       <div data-role="content" style="overflow-x:visible;">
+			       <div class="user" style="height:130px;">
+                <!--实现点击头像或者相机可以更改头像-->
+	                  <div class="avatar">    
+	                     <img src="/xiaolian/xiaolian-v0.1/Public/front/images/mine-user.png" style="clear:both; display: block; margin:auto;">
+	                        <a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/avatar.html" onclick="location.href='/xiaolian/xiaolian-v0.1/index.php/Home/mine/avatar.html'"><button type="button" id="open" style="background:url(/xiaolian/xiaolian-v0.1/Public/front/images/mine-shezhi-cam.png); width:10px; margin:-25px 0 0 160px; border:none;"></button></a>
+	                  </div>
+
+            </div>
+            </div>
+               
+                        <div class="line1">
+                              <img src="/xiaolian/xiaolian-v0.1/Public/front/images/mine-shezhi-uname.png">
+                              <input type="text"  id="zhanghao" class="zhanghao" value="用户名" onfocus="if(value=='用户名') {value=''}" onblur="if (value=='') {value='用户名'}" data-role="none">
+                        </div>
+
+                        <div class="line2">
+                              <div class="ui-grid-a">
+								<div class="ui-block-a">
+									<img src="/xiaolian/xiaolian-v0.1/Public/front/images/mine-shezhi-sex.png" style="float:left; margin:14px 0 0 0;">
+									<label for="sex" style="margin:20px 90px 0 20px;">性别</label>
+								</div>
+							    <div class="ui-block-b" style="margin:0px 0px 0 -50px;">
+									<fieldset>
+										<select name="sex" id="sex" style="heigth:80px;">
+											<option value="boy">男</option>
+											<option value="girl">女</option>
+										</select>
+									</fieldset>
+							    </div>								
+				              </div>
+                        </div>
+
+                        <div class="line2">
+                              <div class="ui-grid-a">
+								<div class="ui-block-a">
+									<img src="/xiaolian/xiaolian-v0.1/Public/front/images/mine-shezhi-birth.png" style="float:left; margin:14px 0 0 0;">
+									<label for="sex" style="margin:20px 90px 0 20px;">生日</label>
+								</div>
+							    <div class="ui-block-b" style="margin:0px 0px 0 -50px;">
+									<input type="date" name="birth" id="birth" >
+							    </div>								
+				              </div>
+                        </div>
+
+                        <div class="line4">
+                               <img src="/xiaolian/xiaolian-v0.1/Public/front/images/mine-shezhi-univer.png">
+                               <input type="text"  id="daxue" class="daxue" value="大学" onfocus="if(value=='大学') {value=''}" onblur="if (value=='') {value='大学'}" data-role="none">
+                        </div>
+
+                        <div class="line5">
+                               <img src="/xiaolian/xiaolian-v0.1/Public/front/images/mine-shezhi-gq.png">
+                               <input type="text"  id="daxue" class="daxue" value="个性签名" onfocus="if(value=='个性签名') {value=''}" onblur="if (value=='') {value='个性签名'}" data-role="none">
+                        </div>
+                        <br/>
+                        <form action="#" method="post"> 			
+				            <a href="#" data-role="button" data-theme="b" id="login" onclick="but_click();" style="background:#6fd7ca; border:none;">提交</a>
+			            </form>
+
+        </div>
+    </div>
+</body>
+</html>
