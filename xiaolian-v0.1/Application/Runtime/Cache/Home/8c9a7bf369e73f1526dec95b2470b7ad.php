979e01a901546bafff04f972927ecf6d043db57b
yushize <928595245@qq.com>
2016-12-14 11:07:56 +0800

    1

--- xiaolian-v0.1/Application/Runtime/Cache/Home/8c9a7bf369e73f1526dec95b2470b7ad.php
+++ xiaolian-v0.1/Application/Runtime/Cache/Home/8c9a7bf369e73f1526dec95b2470b7ad.php
@@ -4,134 +4,52 @@
 	<meta charset="utf-8" />
 	<title>好友</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <link rel="stylesheet" href="/xiaolian/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" /> 
-    <script src="/xiaolian/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
-    <script src="/xiaolian/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
-=======
-    <link rel="stylesheet" href="/xiaolianV0.1/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" /> 
-    <script src="/xiaolianV0.1/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
-    <script src="/xiaolianV0.1/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
-=======
     <link rel="stylesheet" href="/xiaolian-git/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" /> 
     <script src="/xiaolian-git/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
     <script src="/xiaolian-git/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
-<<<<<<< HEAD
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 </head>
 <body>
 	<div data-role="page" >
 		<div data-role="header" style="background-color:#6fd7ca; color:white;">
-			<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/mine.html" data-icon="arrow-l" data-iconpos="notext"></a>
+			<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/mine.html" data-icon="arrow-l" data-iconpos="notext"></a>
 			<h1 style="font-weight:200;">好友</h1>			
 		</div>
 		<div data-role="content">
 			<div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian/xiaolian-v0.1/Public/front/images/haoyou-user.png"></a>
-=======
 				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png"></a>
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
-				<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolianV0.1/xiaolian-v0.1/Public/front/images/haoyou-user.png"></a>
-=======
-				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png"></a>
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 				<p class="name" style="margin: -60px 0 0 80px; font-weight: bold;">liberté</p>
 				<p class="school" style="margin: 10px 0 0 80px; font-size: 14px;">来自河北师范大学</p>
-				<img src="/xiaolian/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
+				<img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
 			</div>
 			<div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
 				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
-				<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolianV0.1/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
-				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 				<p class="name" style="margin: -60px 0 0 80px; font-weight: bold;">liberté</p>
 				<p class="school" style="margin: 10px 0 0 80px; font-size: 14px;">来自河北师范大学</p>
-				<img src="/xiaolian/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
+				<img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
 			</div>
 			<div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
 				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
-				<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolianV0.1/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
-				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 				<p class="name" style="margin: -60px 0 0 80px; font-weight: bold;">liberté</p>
 				<p class="school" style="margin: 10px 0 0 80px; font-size: 14px;">来自河北师范大学</p>
-				<img src="/xiaolian/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
+				<img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
 			</div>
 			<div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
 				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
-				<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolianV0.1/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
-				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 				<p class="name" style="margin: -60px 0 0 80px; font-weight: bold;">liberté</p>
 				<p class="school" style="margin: 10px 0 0 80px; font-size: 14px;">来自河北师范大学</p>
-				<img src="/xiaolian/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
+				<img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
 			</div>
 			<div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
 				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
-				<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolianV0.1/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
-				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 				<p class="name" style="margin: -60px 0 0 80px; font-weight: bold;">liberté</p>
 				<p class="school" style="margin: 10px 0 0 80px; font-size: 14px;">来自河北师范大学</p>
-				<img src="/xiaolian/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
+				<img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
 			</div>
 			<div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-				<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
 				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> aaa9cb512a8ee20eb2f9d804e6bbf75373378db5
-=======
-				<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolianV0.1/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
-=======
-				<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/user-data.html"><img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/haoyou-user.png" style="margin:10px 0 0 0;"></a>
->>>>>>> 605cf8d45551c7c332b7cb95c5813a7eda74bc56
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
 				<p class="name" style="margin: -60px 0 0 80px; font-weight: bold;">liberté</p>
 				<p class="school" style="margin: 10px 0 0 80px; font-size: 14px;">来自河北师范大学</p>
-				<img src="/xiaolian/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
+				<img src="/xiaolian-git/xiaolian-v0.1/Public/front/images/longline.png" style="margin:20px 0 0 -15px;">
 			</div>
 		</div>
 	</div>
--- xiaolian-v0.1/Application/Runtime/Cache/Home/d2a942f20ad5fcc52b6c172e230a27ba.php
+++ xiaolian-v0.1/Application/Runtime/Cache/Home/d2a942f20ad5fcc52b6c172e230a27ba.php
@@ -4,34 +4,14 @@
 	<meta charset="utf-8" />
 	<title>成就</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-<<<<<<< HEAD
     <link rel="stylesheet" href="/xiaolian-git/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" />
     <script src="/xiaolian-git/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
     <script src="/xiaolian-git/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
-=======
-<<<<<<< HEAD
-    <link rel="stylesheet" href="/xiaolian/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" />
-    <script src="/xiaolian/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
-    <script src="/xiaolian/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
-=======
-    <link rel="stylesheet" href="/xiaolianV0.1/xiaolian-v0.1/Public/front/css/jquery.mobile-1.4.5.min.css" />
-    <script src="/xiaolianV0.1/xiaolian-v0.1/Public/front/js/jquery-2.2.2.js"></script>
-    <script src="/xiaolianV0.1/xiaolian-v0.1/Public/front/js/jquery.mobile-1.4.5.min.js"></script>
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
->>>>>>> cae4ba465b91c2614177aa528adf8cc11474469c
 </head>
 <body>
 	<div data-role="page">
 		<div data-role="header" style="background-color:#6fd7ca; color:white;">
-<<<<<<< HEAD
 			<a href="/xiaolian-git/xiaolian-v0.1/index.php/Home/mine/mine.html" data-icon="arrow-l" data-iconpos="notext"></a>
-=======
-<<<<<<< HEAD
-			<a href="/xiaolian/xiaolian-v0.1/index.php/Home/mine/mine.html" data-icon="arrow-l" data-iconpos="notext"></a>
-=======
-			<a href="/xiaolianV0.1/xiaolian-v0.1/index.php/Home/mine/mine.html" data-icon="arrow-l" data-iconpos="notext"></a>
->>>>>>> 01b57ce9fa315e0ea71222318892f143ea1e8fb8
->>>>>>> cae4ba465b91c2614177aa528adf8cc11474469c
 			<h1 style="font-weight:200;">成就</h1>			
 		</div>
 			  <div class="ui-grid-a">
