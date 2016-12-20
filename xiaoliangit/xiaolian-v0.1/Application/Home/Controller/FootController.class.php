d5c64ac756fd37bf871b0573235eb15e77815348
yushize <928595245@qq.com>
2016-12-20 15:14:19 +0800

    1

--- xiaoliangit/xiaolian-v0.1/Application/Home/Controller/FootController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class FootController extends Controller {
-    public function index(){
-       $this->display();
-    }
-   
-}
--- xiaoliangit/xiaolian-v0.1/Application/Home/Controller/TaskController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class TaskController extends Controller {
-    public function index(){
-       $this->display();
-    }
-   
-}
